9738dab2744e075667746a3ae2ab4713e916f06c
noman-swe <noman.swe130@gmail.com>
2022-02-16 20:20:10 +0600

    27-9 (advanced) Nested function and error handling

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,5 @@
 
-function getInputValue(inputId){
+function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
     const inputAmountText = inputField.value;
     const amountValue = parseFloat(inputAmountText);
@@ -9,7 +9,7 @@ function getInputValue(inputId){
 }
 
 // get previous value
-function updateTotalField(totalFieldId, depositAmount){
+function updateTotalField(totalFieldId, depositAmount) {
     //debugger;
     const totalElement = document.getElementById(totalFieldId);
     const totalText = totalElement.innerText;
@@ -17,37 +17,61 @@ function updateTotalField(totalFieldId, depositAmount){
     const amount = previousTotal + depositAmount;
     totalElement.innerText = amount;
 }
-
-// update balance
-function updateBalance(amount, isAdd){
+// get current balance
+function getCurrentBalance(){
     const balanceTotal = document.getElementById('balance-total');
     const balanceText = balanceTotal.innerText;
     const balanceAmount = parseFloat(balanceText);
+    return balanceAmount;
+}
+
+// update balance
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    
+    /* const balanceText = balanceTotal.innerText;
+    const balanceAmount = parseFloat(balanceText); */
+
+    const balanceAmount = getCurrentBalance();
     // new balance from deposit
-    if(isAdd == true){
+    if (isAdd == true) {
         const newTotalBalance = balanceAmount + amount;
-        balanceTotal.innerText = newTotalBalance; 
-    }else{
+        balanceTotal.innerText = newTotalBalance;
+    } else {
         const newTotalBalance = balanceAmount - amount;
-        balanceTotal.innerText = newTotalBalance; 
+        balanceTotal.innerText = newTotalBalance;
     }
 }
 
 // deposit & balance
-document.getElementById('deposit-btn').addEventListener('click', function(){
+document.getElementById('deposit-btn').addEventListener('click', function () {
     // get new deposit 
-   const depositAmount = getInputValue('deposit-input');
-   // get previous deposit
-    updateTotalField('deposit-total', depositAmount);
-    // update Balance section
-    updateBalance(depositAmount, true);
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        // get previous deposit
+        updateTotalField('deposit-total', depositAmount);
+        // update Balance section
+        updateBalance(depositAmount, true);
+    } else {
+        console.log('Input positive value');
+    }
+
 });
 
 // withdraw & balance
-document.getElementById('withdraw-btn').addEventListener('click', function(){
-    const withdrawAmount = getInputValue('withdraw-input');   
-    // show withdraw amount using function
-    updateTotalField('withdraw-total', withdrawAmount);
-    // update balance (balance - withdraw)
-    updateBalance(withdrawAmount, false);
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdrawAmount = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        // show withdraw amount using function
+        updateTotalField('withdraw-total', withdrawAmount);
+        // update balance (balance - withdraw)
+        updateBalance(withdrawAmount, false);
+    }else{
+        console.log('please enter a number');
+    }
+    if(withdrawAmount > currentBalance){
+        console.log('You can not withdraw more than what you have!');
+    }
+
 });
